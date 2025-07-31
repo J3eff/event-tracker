ea92728a370ed86935c324305d320055930a6cc3
J3eff <jeffersonvideo125@gmail.com>
2025-07-31 00:29:53 -0300

    feat: Um mundo feito de átomos

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { IEvento } from "../../interfaces/IEvento";
 import style from "./Calendario.module.scss";
 import ptBR from "./localizacao/ptBR.json";
 import Kalend, { CalendarView } from "kalend";
 import "kalend/dist/styles/index.css";
+import { listaDeEventosState } from "../../state/atom";
+import { useRecoilValue } from "recoil";
 
 interface IKalendEvento {
   id?: number;
@@ -13,8 +14,9 @@ interface IKalendEvento {
   color: string;
 }
 
-const Calendario: React.FC<{ eventos: IEvento[] }> = ({ eventos }) => {
+const Calendario: React.FC = () => {
   const eventosKalend = new Map<string, IKalendEvento[]>();
+  const eventos = useRecoilValue(listaDeEventosState);
 
   eventos.forEach((evento) => {
     const chave = evento.inicio.toISOString().slice(0, 10);
